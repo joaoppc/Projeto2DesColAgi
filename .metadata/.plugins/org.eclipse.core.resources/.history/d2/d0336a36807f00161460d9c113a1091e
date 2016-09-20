33e6ba9799f532e94aa617dfc8c41a6e680e787f
lucassa3 <lucassa3@al.insper.edu.br>
2016-09-20 20:19:21 -0300

    All logic gates DONE. Gate test classes DONE.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/d2/d0336a36807f00161460d9c113a1091e
@@ -0,0 +1,41 @@
+package br.edu.insper.agile.controller;
+
+public class XnorGate extends LogicGate {
+	InputPin pinA;
+	InputPin pinB;
+	public XnorGate(InputPin pinA,InputPin pinB) {
+		this.pinA = pinA;
+		this.pinB = pinB;
+	}
+
+	
+	AndGate and1;
+	AndGate and2;
+	AndGate and3;
+	AndGate and4;
+	
+	
+	@Override
+	public InputPin getInputPin(int index) {
+		if (index == 0) {
+			return pinA;
+		}
+		else {
+			return pinB;
+		}
+		
+	}
+	
+	@Override
+	public boolean getOutputValue(int index) {
+		//podemos construir usando portas and ja existentes
+		and1 = new AndGate(pinA, pinB);
+		and2 = new AndGate(pinA, new InputPin(and1,0)); //jeito mais esperto de fazer, mas nao sei se da certo
+		and3 = new AndGate(pinB, new InputPin(new Switch(!and1.getOutputValue(0)),0)); //jeito burro de fazer, mas tem que dar certo
+		and4 = new AndGate(new InputPin(new Switch(!and2.getOutputValue(0)),0),new InputPin(new Switch(!and3.getOutputValue(0)),0));
+		
+		return and4.getOutputValue(index);
+		
+	}
+
+}
---
+++ PortasLogicas/test/br/edu/insper/agile/controller/XorGateTest.java
@@ -0,0 +1,65 @@
+package br.edu.insper.agile.controller;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class XorGateTest {
+	Switch botaoA;
+	Switch botaoB;
+	InputPin pinA;
+	InputPin pinB;
+
+	@Test
+	public void falseFalse() {
+		botaoA = new Switch(false);
+		botaoB = new Switch(false);
+		
+		pinA = new InputPin(botaoA,0);
+		pinB = new InputPin(botaoB,1);
+		
+		XorGate xor = new XorGate(pinA,pinB);
+		
+		Assert.assertEquals(false, xor.getOutputValue(0));
+	}
+	
+	@Test
+	public void trueFalse() {
+		botaoA = new Switch(true);
+		botaoB = new Switch(false);
+		
+		pinA = new InputPin(botaoA,0);
+		pinB = new InputPin(botaoB,1);
+		
+		XorGate xor = new XorGate(pinA,pinB);
+		
+		Assert.assertEquals(true, xor.getOutputValue(0));
+	}
+	
+	@Test
+	public void falseTrue() {
+		botaoA = new Switch(false);
+		botaoB = new Switch(true);
+		
+		pinA = new InputPin(botaoA,0);
+		pinB = new InputPin(botaoB,1);
+		
+		XorGate xor = new XorGate(pinA,pinB);
+		
+		Assert.assertEquals(true, xor.getOutputValue(0));
+	}
+
+	@Test
+	public void trueTrue() {
+		botaoA = new Switch(true);
+		botaoB = new Switch(true);
+		
+		pinA = new InputPin(botaoA,0);
+		pinB = new InputPin(botaoB,1);
+		
+		XorGate xor = new XorGate(pinA,pinB);
+		
+		Assert.assertEquals(false, xor.getOutputValue(0));
+	}
+}
+
+
